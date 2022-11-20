46e64ed24c03e398e1e097252fb1b97c7e773552
Ander <adb35@alu.ua.es>
2022-11-20 18:07:19 +0100

    Hello World

---
+++ 01-Hello-World/src/main.rs
@@ -0,0 +1,15 @@
+
+// Funcion main
+fn main() {
+    println!("Hello, world!");
+}
+
+
+// Para construir y ejecutar el proyecto
+// cargo build
+//  ./target/debug/Hello-World
+//
+//
+// Para ejecutar unicamente el main:
+// rustc main.rs
+// ./main
