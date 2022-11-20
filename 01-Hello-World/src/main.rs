e2554f489db7b74a643168d2124200ce4e05690a
Ander <adb35@alu.ua.es>
2022-11-20 19:27:25 +0100

    01 Hello World

--- 01-Hello-World/src/main.rs
+++ 01-Hello-World/src/main.rs
@@ -1,15 +1,265 @@
-
 // Funcion main
 fn main() {
     println!("Hello, world!");
+
+    // This is an example of a line comment
+    // There are two slashes at the beginning of the line
+    // And nothing written inside these will be read by the compiler
+
+    // println!("Hello, world!");
+
+    // Run it. See? Now try deleting the two slashes, and run it again.
+
+    /*
+     * This is another type of comment, a block comment. In general,
+     * line comments are the recommended comment style. But
+     * block comments are extremely useful for temporarily disabling
+     * chunks of code. /* Block comments can be /* nested, */ */
+     * so it takes only a few keystrokes to comment out everything
+     * in this main() function. /*/*/* Try it yourself! */*/*/
+     */
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+
+    // You can manipulate expressions more easily with block comments
+    // than with line comments. Try deleting the comment delimiters
+    // to change the result:
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+    // Formatted print
+    // En general {} se reemplaza con los argumentos
+    println!("{} days", 31);
+
+    // Se pueden usar posiciones
+    println!("{0}, this is {1}, {1}, this is {0}", "Alice", "Bob");
+
+    // Tambien se pueden utilizar nombres para argumentos
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    // Distintos formatos de print
+    println!("Base 10:               {}", 69420); //69420
+    println!("Base 2 (binary):       {:b}", 69420); //10000111100101100
+    println!("Base 8 (octal):        {:o}", 69420); //207454
+    println!("Base 16 (hexadecimal): {:x}", 69420); //10f2c
+    println!("Base 16 (hexadecimal): {:X}", 69420); //10F2C
+
+    // Justificado del texto a la derecha. 5 espacios. output: "    1"
+    println!("{number:>5}", number = 1);
+
+    // You can pad numbers with extra zeroes,
+    //and left-adjust by flipping the sign. This will output "10000".
+    println!("{number:0<5}", number = 1);
+
+    // You can use named arguments in the format specifier by appending a `$`
+    println!("{number:0>width$}", number = 1, width = 5);
+
+    // Rust comprueba el numero de argumentos es correcto
+    //println!("My name is {0}, {1} {0}", "Bond", "");
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // Solamente los tipos que implementan fmt::Display se pueden
+    // formatear con {}. Los tipos del usuario no implementan
+    // fmt::Display por defecto
+
+    //#[allow(dead_code)]
+    //struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement
+    // fmt::Display
+    //println!("This struct `{}` won't print...", Structure(3));
+    // TODO ^ Try uncommenting this line
+
+    // For Rust 1.58 and above, you can directly capture the argument from a
+    // surrounding variable. Just like the above, this will output
+    // "     1". 5 white spaces and a "1".
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:>width$}");
+
+    // Excercises
+    let pi = 3.141592;
+    let precision = 3;
+    println!("{pi:>.precision$}");
+
+    ///////////////////////////////
+    // DEBUG
+    ///////////////////////////////
+    // This structure cannot be printed either with `fmt::Display` or
+    // with `fmt::Debug`.
+    struct UnPrintable(i32);
+
+    // The `derive` attribute automatically creates the implementation
+    // required to make this `struct` printable with `fmt::Debug`.
+    #[derive(Debug)]
+    struct DebugPrintable(i32);
+
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    // Put a `Structure` inside of the structure `Deep`. Make it printable
+    // also.
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    ///////////////////////////////
+    // Display
+    ///////////////////////////////
+    //
+
+    use std::fmt;
+
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
+
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})", self.0, self.1)
+        }
+    }
+
+    // Define a structure where the fields are nameable for comparison.
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    // Similarly, implement `Display` for `Point2D`
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Customize so only `x` and `y` are denoted.
+            write!(f, "x: {}, y: {}", self.x, self.y)
+        }
+    }
+
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    ///////////////////////////////
+    // Testcase: List
+    ///////////////////////////////
+    ///
+    // Define a structure named `List` containing a `Vec`.
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Extract the value using tuple indexing,
+            // and create a reference to `vec`.
+            let vec = &self.0;
+
+            write!(f, "[")?;
+
+            // Iterate over `v` in `vec` while enumerating the iteration
+            // count in `count`.
+            for (count, v) in vec.iter().enumerate() {
+                // For every element except the first, add a comma.
+                // Use the ? operator to return on errors.
+                if count != 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{}:{}", count, v)?;
+            }
+
+            // Close the opened bracket and return a fmt::Result value.
+            write!(f, "]")
+        }
+    }
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    ///////////////////////////////
+    // Formatting
+    ///////////////////////////////
+
+    use std::fmt::{Display, Formatter};
+
+    struct City {
+        name: &'static str,
+        // Latitude
+        lat: f32,
+        // Longitude
+        lon: f32,
+    }
+
+    impl Display for City {
+        // `f` is a buffer, and this method must write the formatted string into it
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` is like `format!`, but it will write the formatted string
+            // into a buffer (the first argument)
+            write!(
+                f,
+                "{}: {:.3}°{} {:.3}°{}",
+                self.name,
+                self.lat.abs(),
+                lat_c,
+                self.lon.abs(),
+                lon_c
+            )
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", *color);
+    }
 }
-
-
-// Para construir y ejecutar el proyecto
-// cargo build
-//  ./target/debug/Hello-World
-//
-//
-// Para ejecutar unicamente el main:
-// rustc main.rs
-// ./main
